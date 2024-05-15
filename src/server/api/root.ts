8cd1485ad56770a47c223316490d1e2227a77914
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-05-15 21:52:27 +0530

    added root router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,11 @@
-import { postRouter } from "~/server/api/routers/post";
+import { ideaRouter } from "~/server/api/routers/idea";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { ideaEssenceRouter } from "~/server/api/routers/idea-essence";
+import { personaRouter } from "~/server/api/routers/persona";
+import { questionRouter } from "~/server/api/routers/question";
+import { responseRouter } from "~/server/api/routers/response";
+import { surveyRouter } from "~/server/api/routers/survey";
+import { openAIRouter } from "./routers/openai";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +13,13 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  idea: ideaRouter,
+  ideaEssence: ideaEssenceRouter,
+  persona: personaRouter,
+  question: questionRouter,
+  response: responseRouter,
+  survey: surveyRouter,
+  openAi: openAIRouter
 });
 
 // export type definition of API
